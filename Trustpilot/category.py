086383033550f7604acbaaf69982f7d526aaceba
Sabbir Ahmad <107953870+Sabbirbracu@users.noreply.github.com>
2024-07-03 15:41:22 +0600

    Update category.py

--- Trustpilot/category.py
+++ Trustpilot/category.py
@@ -11,29 +11,21 @@ webdriver_path = '/Users/sabbirahmad/Desktop/chromedriver'
 service = Service(webdriver_path)
 options = Options()
 driver = webdriver.Chrome(service=service, options=options)
-
-# Open the Trustpilot categories page
 driver.get('https://www.trustpilot.com/categories')
 
-# List to store sub-categories
 categories = []
-
-# Locate all sub-categories on the page
 sub_category_elements = driver.find_elements(By.XPATH, '//li[@class="styles_linkItem__KtBm6"]/a')
 
 for sub_category_element in sub_category_elements:
     sub_category_name = sub_category_element.text
     categories.append(sub_category_name)
 
-# Close the web driver
 driver.quit()
 
-# Format the output as {"Categories": []}
 output = {"Categories": categories}
 
-# Print the categories dictionary
 print(json.dumps(output, indent=4))
 
-# Optionally, save to a JSON file
+#  save to a JSON file and please provide the path where you want to save it
 with open('/Users/sabbirahmad/Trustpilot/categories.json', 'w') as json_file:
     json.dump(output, json_file, indent=4)
