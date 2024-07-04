9883bdba9a20e278e702d84e05037ae277d74012
Sabbir Ahmad <107953870+Sabbirbracu@users.noreply.github.com>
2024-07-04 15:47:43 +0600

    Update urls.py
    More faster

--- Trustpilot/urls.py
+++ Trustpilot/urls.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-
+logger = logging.getLogger()
 # Path to your webdriver executable
 webdriver_path = '/Users/sabbirahmad/Desktop/chromedriver'
 
@@ -31,7 +31,7 @@ json_file_path = '/Users/sabbirahmad/Trustpilot/categories.json'
 # Initialize Chrome web driver with existing user profile
 chrome_options = Options()
 chrome_options.add_argument("/Users/sabbirahmad/Library/Application Support/Google/Chrome/Default")  # Change to your Chrome profile path
-# chrome_options.add_argument("--headless")  # Optional: Run Chrome in headless mode, i.e., without a UI
+chrome_options.add_argument("--headless")  # Optional: Run Chrome in headless mode, i.e., without a UI
 service = Service(webdriver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
@@ -48,11 +48,17 @@ def is_pagination_visible(driver):
     except:
         return False
 
-# Function to check if pagination button is interactable
 def is_pagination_button_interactable(driver):
     try:
         pagination_button = driver.find_element(By.XPATH, '//div[@class="styles_paginationWrapper__fukEb styles_pagination__USObu"]/nav[@aria-label="Pagination"]/a[@name="pagination-button-next"]')
-        return pagination_button.is_enabled() and pagination_button.get_attribute("aria-disabled") != "true"
+        if pagination_button.get_attribute("aria-disabled") == "true":
+            logger.info("Pagination is end")
+            return False
+        elif pagination_button.get_attribute("data-pagination-button-next-link") == "true":
+            logger.info("Next page is available")
+            return True
+        else:
+            return False
     except:
         return False
 
@@ -91,7 +97,7 @@ def main(driver, sub_category, location, Rattings):
     driver.get(url)
 
     # Wait for the main content to load
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 2)
     try:
         wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="styles_main__XgQiu"]')))
         print(f"Main content loaded for {sub_category}.")
@@ -106,17 +112,17 @@ def main(driver, sub_category, location, Rattings):
         if is_pagination_visible(driver) and is_pagination_button_interactable(driver):
             print("Pagination button found and interactable. Clicking it.")
             try:
-                # Wait for the pagination button to be clickable
+               
                 pagination_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class="styles_paginationWrapper__fukEb styles_pagination__USObu"]/nav[@aria-label="Pagination"]/a[@name="pagination-button-next"]')))
-
-                # Scroll to the pagination button
                 driver.execute_script("arguments[0].scrollIntoView(true);", pagination_button)
 
                 # Click the pagination button using ActionChains
                 ActionChains(driver).move_to_element(pagination_button).click().perform()
+                # Attempt to click using JavaScript
+                driver.execute_script("arguments[0].click();", pagination_button)
+                logger.info("Clicked the pagination button")
+                time.sleep(1)
 
-                # Wait for the next page to load
-                time.sleep(5)
             except Exception as e:
                 print(f"Error clicking pagination button: {e}")
                 break
