324d890c913992bf2b605b36c08e42f9cd3c0c74
Sabbir Ahmad <107953870+Sabbirbracu@users.noreply.github.com>
2024-07-08 22:02:27 +0600

    Update url.py

--- Google Maps/url.py
+++ Google Maps/url.py
@@ -12,15 +12,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys    
-webdriver_path = '/Users/sabbirahmad/Desktop/chromedriver'
-category = 'Sabbir'
+webdriver_path = os.getenv("WEBDRIVER_PATH")
+category = 'Burger'
 location = 'Berlin'
 source = 'https://www.google.com/maps'
 source_name = "Google"
-csv_file_path = '/Users/sabbirahmad/google maps/Pizza in Germany.csv'
+csv_file_path = os.getenv("CSV_FILE_PATH_for_google")
 python_executable = sys.executable  # This gets the currently running Python executable
 try:
-    subprocess.run([python_executable, '/Users/sabbirahmad/google maps/details.py', csv_file_path, source_name, source, category], check=True)
+    subprocess.run([python_executable, os.getenv("DETAIL_PATH_for_google") , csv_file_path, source_name, source, category], check=True)
     print(f"details.py executed successfully with {csv_file_path}")
 except subprocess.CalledProcessError as e:
     print(f"Error calling details.py: {e}")
